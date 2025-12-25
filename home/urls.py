6ba80f82ec146820df3f9db5a1f02fdf3d6f6640
abdullohsajfuddinov4-maker <abdullohsajfuddinov4@gmail.com>
2025-12-25 13:49:25 +0500

    full masALA

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home,project,page_404
 
 
 urlpatterns =[
-    path('',home,name='home')
+    path('',home,name='home'),
+    path('page_404',page_404,name='page_404'),
+    path('project/<int:pk>',project,name='project')
+
 ]
